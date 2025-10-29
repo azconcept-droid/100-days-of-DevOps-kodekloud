5f2cb6bc00d351a8fa0a392d411d7a8681999684
Yahaya Azeez <azconcept2016@gmail.com>
2025-10-29 14:45:59 +0100

    fix redis bug on kubernetes cluster

---
+++ day-59/commands.sh
@@ -0,0 +1,6 @@
+k get pods
+k describe pods
+k get configmaps
+kubectl edit deployment redis-deployment # correct typo conig --> config, redis:alpin --> redis:alpine
+kubectl rollout status deployment redis-deployment
+k get pods
